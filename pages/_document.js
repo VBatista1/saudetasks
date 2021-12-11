a7ba0c0de5b9f423639d8ddac07b0050362b0212
Victor Batista <victor.batista1@hotmail.com>
2021-12-11 02:26:20 -0300

    Adicionado babel-plugin-styled-components e feito adaptação para styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { ServerStyleSheet } from 'styled-components';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
   }
 
   render() {
     return (
       <Html>
         <Head>
+          {this.props.styleTags}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
