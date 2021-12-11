b6e26f6e0b2874964d45f31c41910e12bdf872fa
Victor Batista <victor.batista1@hotmail.com>
2021-12-11 00:54:36 -0300

    Implementado GlobalStyle e adicionado variáveis das cores

---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+:root {
+    --black: #000;
+    --white: #fff;
+    --light-grey: #F5F6FA;
+    --light-blue: #2E5763;
+    --dark-blue: #13414D;
+  }
+
+`;
