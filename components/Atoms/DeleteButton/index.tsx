6dda436519791d2267097166f4711e99652277b2
Victor Batista <victor.batista1@hotmail.com>
2021-12-13 16:19:11 -0300

    Criado botao de deletar task

---
+++ components/Atoms/DeleteButton/index.tsx
@@ -0,0 +1,10 @@
+import { DeleteButtonContainer } from './styles'
+import DeleteIcon from "public/icons/delete_icon.svg";
+
+interface DeleteButtonProps {
+    onClick: () => void;
+}
+
+export const DeleteButton: React.FC<DeleteButtonProps> = ({onClick}) => {
+  return <DeleteButtonContainer onClick={onClick}><DeleteIcon fill="var(--red)"/></DeleteButtonContainer>;
+}
---
+++ components/Atoms/DeleteButton/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const DeleteButtonContainer = styled.button`
+  svg {
+      height: 25px;
+      width: 25px;
+  }
+`;
