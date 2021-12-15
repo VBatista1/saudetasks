4406b4bbe15d10a46f6cfdb97a24bb1cae38b72b
Victor Batista <victor.batista1@hotmail.com>
2021-12-15 03:34:05 -0300

    Alterado tipo de display do home e navbar para display grid

--- components/Organisms/Navbar/styles.ts
+++ components/Organisms/Navbar/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const NavbarContainer = styled.nav`
+  grid-area: navbar
   width: 100%;
-  height: 60px;
   box-shadow: rgb(149 157 165 / 20%) 0px 8px 24px;
   padding: 10px 20px;
   background: white;
