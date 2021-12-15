bb4b154b1ffaa91705b9fe761b3b2918f101590f
Victor Batista <victor.batista1@hotmail.com>
2021-12-15 03:29:31 -0300

    Criado logica do componente Greeting, para pegar o nome do usuario e seu genero

--- components/Molecules/Greeting/index.tsx
+++ components/Molecules/Greeting/index.tsx
@@ -1,10 +1,14 @@
+import { selectUser } from "redux/features/user/userSlice";
 import { GreetingContainer } from "./styles";
+import { useAppSelector } from "redux/hooks";
 
 export const Greeting: React.FC = () => {
+  const { user } = useAppSelector(selectUser);
+
   return (
     <GreetingContainer>
-      <h2>Bom dia !</h2>
-      <p>Dayaananda Sethi</p>
+      <h2>{user.gender === 'male' ? 'Seja bem-vindo !' : 'Seja bem-vinda !'}</h2>
+      <p>{user.name}</p>
     </GreetingContainer>
   );
 };
--- components/Molecules/Greeting/styles.ts
+++ components/Molecules/Greeting/styles.ts
@@ -2,7 +2,10 @@ import styled from "styled-components";
 
 export const GreetingContainer = styled.div`
   color: var(--dark-blue);
-  margin: 20px;
+  margin: 40px 20px;
+
   p {
+    font-size: 1.20rem;
+    margin-top: 5px;
   }
 `;
