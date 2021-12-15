54e263d7f82592b3253a166e37003d206d622b15
Victor Batista <victor.batista1@hotmail.com>
2021-12-15 03:32:31 -0300

    Alterado logica da modal de criacao e edicao de task

--- components/Organisms/ModalTask/index.tsx
+++ components/Organisms/ModalTask/index.tsx
@@ -1,76 +1,64 @@
 import { Button } from "components/Atoms/Button";
 import { DatePicker } from "components/Atoms/DatePicker";
-import { DeleteButton } from "components/Atoms/DeleteButton";
 import { TextArea } from "components/Atoms/TextArea";
-import { ITask } from "components/Molecules/TaskCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
-import { createTask, deleteTask, editTask } from "services/tasks";
-import { ModalTaskContainer, ModalTaskContent, ModalHeader } from "./styles";
+import { createTask, editTask } from "services/tasks";
+import { ModalHeader } from "./styles";
 import LoadingIcon from "public/icons/loading.svg";
-
-const initialTask = {
-  due_on: "",
-  id: 0,
-  status: "",
-  title: "",
-  user_id: 0,
-};
-
-interface ModalTaskProps {
-  open: boolean;
-  variant: string;
-  closeModal: () => void;
-  task?: ITask;
-}
-
-export const ModalTask: React.FC<ModalTaskProps> = ({
-  open,
+import { useAppSelector, useAppDispatch } from "redux/hooks";
+import {
   closeModal,
-  variant,
-  task = initialTask,
-}) => {
-  const [title, setTitle] = useState(task.title);
-  const [due_on, setDue_on] = useState(task.due_on);
-  const [ loading, setLoading ] = useState(false)
-  
+  selectModal,
+  updateTask,
+} from "redux/features/modal/modalSlice";
+import { Modal } from "components/Molecules/Modal";
+import { ModalDelete } from "../ModalDelete";
+import { addTask, modifyTask } from "redux/features/tasks/tasksSlice";
+
+export const ModalTask: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { variant, task, open } = useAppSelector(selectModal);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const buttonLabel = variant === "new" ? "Criar Task" : "Salvar Task";
 
   const handleDue_onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDue_on(event.target.value);
+    dispatch(updateTask({ due_on: event.target.value }));
   };
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTitle(event.target.value);
+    dispatch(updateTask({ title: event.target.value }));
   };
 
-  const cleanModal = () => {
-    setTitle("");
-    setDue_on("");
+  const handleCloseModal = () => {
     setLoading(false);
-  }
+    dispatch(closeModal());
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.stopPropagation();
     event.preventDefault();
 
     try {
       setLoading(true);
       if (variant === "new") {
-        await createTask(title, due_on);
+        const response = await createTask(task.title, task.due_on);
+        dispatch(addTask(response.data));
         toast.success("Task criada com sucesso");
-        cleanModal();
-        closeModal();
+        handleCloseModal();
       } else {
-        await editTask({
+        const newTask = {
           id: task.id,
-          title: title,
-          due_on: due_on,
+          title: task.title,
+          due_on: task.due_on,
           status: task.status,
           user_id: task.user_id,
-        });
+        }
+        await editTask(newTask);
+        dispatch(modifyTask(newTask));
         toast.success("Task editada com sucesso");
-        cleanModal();
-        closeModal();
+        handleCloseModal();
       }
     } catch (err: any) {
       toast.error(err);
@@ -78,47 +66,31 @@ export const ModalTask: React.FC<ModalTaskProps> = ({
     }
   };
 
-  const handleDelete = async () => {
-    setLoading(true);
-    try{
-      await deleteTask(task.id);
-      toast.success("Task deletada com sucesso");
-      cleanModal();
-      closeModal();
-    } catch (err: any) {
-      toast.error(err);
-      setLoading(false);
-    }
-  }
+  
 
   return (
-    <ModalTaskContainer open={open} onClick={closeModal}>
-      <ModalTaskContent
-        onClick={(event: React.MouseEvent<HTMLElement>) => {
-          event.stopPropagation();
-        }}
-        onSubmit={handleSubmit}
-      >
+    <Modal open={open} close={handleCloseModal}>
+      <form onSubmit={handleSubmit}>
         <ModalHeader>
           <h2>{variant === "new" ? "Criar Nova Task" : "Editar Task"}</h2>
-          {variant !== "new" ? <DeleteButton onClick={handleDelete}/> : null}
+          {variant !== "new" ? <ModalDelete/> : null}
         </ModalHeader>
         <TextArea
           label="Descrição"
-          value={title}
+          value={task.title}
           onChange={handleTitleChange}
           required
         />
         <DatePicker
           label="Data de Término"
-          value={due_on.toString().slice(0, 23)}
+          value={task.due_on.toString().slice(0, 23)}
           onChange={handleDue_onChange}
           required
         />
         <Button type="submit">
-          {loading ? <LoadingIcon height={30} width={30}/> : buttonLabel}
+          {loading ? <LoadingIcon height={30} width={30} /> : buttonLabel}
         </Button>
-      </ModalTaskContent>
-    </ModalTaskContainer>
+      </form>
+    </Modal>
   );
 };
--- components/Organisms/ModalTask/styles.ts
+++ components/Organisms/ModalTask/styles.ts
@@ -1,34 +1,13 @@
 import styled from "styled-components";
 
-interface ModalTaskContainerProps {
-  open: boolean;
-}
-
-export const ModalTaskContainer = styled.div`
-  display: ${(props: ModalTaskContainerProps) => props.open ? "block" : "none" };
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.4);
-  padding: 0 20px;
-`;
-
-export const ModalTaskContent = styled.form`
-  background-color: #fefefe;
-  margin: 15% auto;
-  padding: 20px;
-  max-width: 600px;
-  border-radius: 15px;
-  color: var(--dark-blue);
-`;
-
 export const ModalHeader = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
+`;
+
+export const ModalFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
 `;
